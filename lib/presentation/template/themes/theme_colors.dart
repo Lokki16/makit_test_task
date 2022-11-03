770c025b21db0e18114c3c563ffe72084535eb17
Yuki <52448072+Lokki16@users.noreply.github.com>
2022-11-04 05:23:46 +0600

    Update theme_colors.dart

--- lib/presentation/template/themes/theme_colors.dart
+++ lib/presentation/template/themes/theme_colors.dart
@@ -3,14 +3,23 @@ import 'package:makit_test_task/presentation/template/template.dart';
 class ThemeColors {
   // transparent
   static const transparent = Color(0x00000000);
+  static const transparent2 = Color(0x0000001a);
 
   // white
   static const white = Color(0xFFFFFFFF);
 
   // black
   static const black = Color(0xFF000000);
+  static const black2 = Color(0xFF3A3A3A);
+  static const black3 = Color(0xFF828282);
 
   // grey
-  static const grey = Color(0xFFC3C3C3);
-  static const grey2 = Color(0xFF929292);
+  static const grey = Color(0xFF8A8A8F);
+  static const grey2 = Color(0xFF999999);
+  static const grey3 = Color(0xFFBBBBBD);
+  static const grey4 = Color(0xFFD8D8D8);
+
+  // purple
+  static const purple = Color(0xFF9B51E0);
+  static const purple2 = Color(0xFFBB6BD9);
 }
